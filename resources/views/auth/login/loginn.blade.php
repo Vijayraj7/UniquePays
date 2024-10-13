66a79a28419ed72722922998907d56ee7de6cb6d
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2024-10-13 16:00:05 +0530

    Update loginn.blade.php

--- resources/views/auth/login/loginn.blade.php
+++ resources/views/auth/login/loginn.blade.php
@@ -77,10 +77,7 @@
                                 </a>
                             </div>
                             <!-- /Logo -->
-                            <h4 class="mb-2">Welcome t!</h4>
-                            <h4 class="mb-2">Welcome t!</h4>
-                            <h4 class="mb-2">Welcome t!</h4>
-                            <h4 class="mb-2">Welcom!</h4>
+                            <h4 class="mb-2">Welcome to GMS!</h4>
                             <p class="mb-4">
                                 Please sign-in to your account and
                                 make the profit.
